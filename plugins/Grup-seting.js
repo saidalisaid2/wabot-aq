51e8ae50c348e1e9e5b0dfce7c189b59dc03d486
saidalisaid2 <86299543+saidalisaid2@users.noreply.github.com>
2021-07-19 21:34:22 +0530

    Update Grup-seting.js

--- plugins/Grup-seting.js
+++ plugins/Grup-seting.js
@@ -7,7 +7,7 @@ let handler  = async (m, { conn, args, usedPrefix, command }) => {
 	await conn.updatePresence(m.chat, Presence.composing)
 	if (isClose === undefined)
 		throw `
-*Format salah! Contoh :*
+*തെറ്റായ ഫോർമാറ്റ് anunnenba! ഉദാഹരണം :*
 
   *○ ${usedPrefix + command} close*
   *○ ${usedPrefix + command} open*
