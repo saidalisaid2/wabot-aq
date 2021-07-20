77eeb7adc63c685b6f6ac9ebb90402249e349201
saidalisaid2 <86299543+saidalisaid2@users.noreply.github.com>
2021-07-20 21:06:43 +0530

    Update add.js

--- plugins/add.js
+++ plugins/add.js
@@ -18,7 +18,7 @@ let handler = async (m, { conn, text, participants }) => {
       invite_code,
       invite_code_exp
     }]] = Object.entries(user)
-    let teks = `Mengundang @${jid.split('@')[0]} menggunakan invite...`
+    let teks = `ക്ഷണിക്കുന്നു @${jid.split('@')[0]} ക്ഷണങ്ങൾ ഉപയോഗിക്കുന്നു....`
     m.reply(teks, null, {
       contextInfo: {
         mentionedJid: conn.parseMention(teks)
