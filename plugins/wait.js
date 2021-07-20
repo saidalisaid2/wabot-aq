c5435378b07c453316ae296cc712b251f57e4b7b
saidalisaid2 <86299543+saidalisaid2@users.noreply.github.com>
2021-07-20 17:41:27 +0530

    Update wait.js

--- plugins/wait.js
+++ plugins/wait.js
@@ -5,7 +5,7 @@ const fetch = require('node-fetch')
 let handler = async (m, { conn, usedPrefix }) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw `Reply Foto/Kirim Foto Dengan Caption ${usedPrefix}wait`
+  if (!mime) throw `Reply photo/ഇതിനൊപ്പം ഫോട്ടോ അയയ്‌ക്കുക Caption ${usedPrefix}wait`
   if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`
   let img = await q.download()
   await m.reply('Searching Anime Titles...')
@@ -17,18 +17,18 @@ let handler = async (m, { conn, usedPrefix }) => {
     },
     body: JSON.stringify({ image: anime }),
   })
-  if (!response.ok) throw 'Gambar tidak ditemukan!'
+  if (!response.ok) throw 'ചിത്രം കണ്ടെത്തിയില്ല!'
   let result = await response.json()
   let { is_adult, title, title_chinese, title_romaji, episode, season, similarity, filename, at, tokenthumb, anilist_id } = result.docs[0]
   let link = `https://media.trace.moe/video/${anilist_id}/${encodeURIComponent(filename)}?t=${at}&token=${tokenthumb}`
   let nobuyaki = `
-${similarity < 0.89 ? 'Saya Memiliki Keyakinan Rendah Tentang Hal Ini' : ''}
+${similarity < 0.89 ? 'എനിക്ക് ഇതിനെക്കുറിച്ച് ആത്മവിശ്വാസം കുറവാണ്' : ''}
 
-❏ Judul Jepang : *${title}*
-❏ Ejaan Judul : *${title_romaji}*
-❏ Similarity : *${(similarity * 100).toFixed(1)}%*
-❏ Episode : *${episode.toString()}*
-❏ Ecchi : *${is_adult ? 'Yes' : 'No'}*
+❏ ജാപ്പനീസ് ശീർഷകം : *${title}*
+❏ അക്ഷരവിന്യാസം : *${title_romaji}*
+❏ സമാനത : *${(similarity * 100).toFixed(1)}%*
+❏ എപ്പിസോഡ് : *${episode.toString()}*
+❏ ഇച്ചി : *${is_adult ? 'Yes' : 'No'}*
 `.trim()
   conn.sendFile(m.chat, link, 'srcanime.mp4', `${nobuyaki}`, m)
 }
